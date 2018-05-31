f4e3c90bde730b9b619f0e4a45768e540f1fa730
杨杰 <jie.yang@shengyouxi.com>
2018-05-31 15:34:25 +0800

    no message

--- app/Http/Controllers/Api/BooksController.php
+++ app/Http/Controllers/Api/BooksController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class BooksController extends Controller
 {
     public function index(){
-        $books = Book::orderBy("updated_at","desc")->paginate(4);
+        $books = Book::orderBy("updated_at","desc")->paginate(20);
         return $this->response->paginator($books,new BookTransformer());
     }
 }
--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        \App\Providers\EasySmsServiceProvider::class,
+        \App\Providers\EasySmsServiceProvider::class
     ],
 
     /*
